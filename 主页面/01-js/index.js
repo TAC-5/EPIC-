62341eab493a2928d01c67212befb722a7bc8ab7
TAC-5 <3467913546@qq.com>
2020-04-24 22:11:24 +0800

    EPIC项目Day-3

--- 主页面/01-js/index.js
+++ 主页面/01-js/index.js
@@ -1,3 +1,8 @@
+// 所有界面店家返回顶部就回到顶部
 $('.right-goTop').on('click',function () {
     $(document).scrollTop(0)
 }) 
+// 帮助页面点击X按钮后该行话消失
+$(".clear").click(function () {
+    $('.out').animate({height:0,opacity:0},1200)
+})
