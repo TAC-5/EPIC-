29846657857215687029335ec972aaaff36a0526
TAC-5 <3467913546@qq.com>
2020-04-28 07:16:40 +0800

    EPIC项目Day-5

--- 主页面/01-js/index.js
+++ 主页面/01-js/index.js
@@ -1,8 +1,22 @@
-// 所有界面店家返回顶部就回到顶部
+// 所有界面点击返回顶部就回到顶部
 $('.right-goTop').on('click',function () {
     $(document).scrollTop(0)
 }) 
 // 帮助页面点击X按钮后该行话消失
 $(".clear").click(function () {
     $('.out').animate({height:0,opacity:0},1200)
+})
+// 帮助页面鼠标移入图片图片高亮放大
+$('.big').on('mouseenter',function () {
+    for(var i = 0; i < $('.big').length; i++) {
+        $('big').eq(i).addClass("op")
+        $(this).addClass('in')
+    }
+})
+
+$('.big').on('mouseleave',function () {
+    for(var i = 0; i < $('.big').length; i++) {
+        $('big').eq(i).removeClass("op")
+        $(this).removeClass('in')
+    }
 })
